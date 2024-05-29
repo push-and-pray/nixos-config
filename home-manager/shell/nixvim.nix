26694088315a0d306eadb6f37a47a75c8444b313
ketamin00 <62392537+ketamin00@users.noreply.github.com>
2024-05-29 21:05:28 +0200

    changed cmp mappings

--- home-manager/shell/nixvim.nix
+++ home-manager/shell/nixvim.nix
@@ -34,23 +34,23 @@
           nil_ls.enable = true;
         };
       };
-
+      
       cmp = {
         enable = true;
 	settings = {
 	  mapping = {
-          "<UP>" = "cmp.mapping.select_prev_item()";
-          "<DOWN>" = "cmp.mapping.select_next_item()";
-          "<C-UP>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-DOWN>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-e>" = "cmp.mapping.close()";
-          "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-e>" = "cmp.mapping.close()";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+	    "<C-d>" = "cmp.mapping.scroll_docs(4)";
+	    "<C-f>" = "cmp.mapping.scroll_docs(-4)";
 	  };
 	  sources = [
-            {"name" = "nvim_lsp"; }
-            {"name" = "path"; }
-            {"name" = "buffer"; }
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
 	  ];
 	};
       };
