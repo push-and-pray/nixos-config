0c9ef1c43b28c9bee8bbf92e75ae1a3340ccd0b2
ketamin00 <62392537+ketamin00@users.noreply.github.com>
2024-05-30 17:55:30 +0200

    improved nixvim

--- home-manager/shell/nixvim.nix
+++ home-manager/shell/nixvim.nix
@@ -14,9 +14,21 @@
 
   programs.nixvim = {
     enable = true;
+    extraPlugins = with pkgs.vimPlugins; [
+      {
+        plugin = guess-indent-nvim;
+        config = ''
+          lua require('guess-indent').setup {}
+        '';
+      }
+    ];
+
     opts = {
       number = true;
       relativenumber = true;
+      expandtab = true;
+      copyindent = true;
+      breakindent = true;
     };
     defaultEditor = true;
     clipboard = {
@@ -25,7 +37,12 @@
     };
 
     plugins = {
-      treesitter.enable = true;
+      treesitter = {
+        enable = true;
+        indent = true;
+      };
+
+      lightline.enable = true;
 
       lsp = {
         enable = true;
@@ -34,32 +51,48 @@
           nil_ls.enable = true;
         };
       };
-      
+      lsp-lines = {
+        enable = true;
+        currentLine = true;
+      };
+
       cmp = {
         enable = true;
-	settings = {
-	  mapping = {
+        settings = {
+          mapping = {
             "<C-Space>" = "cmp.mapping.complete()";
             "<C-e>" = "cmp.mapping.close()";
             "<CR>" = "cmp.mapping.confirm({ select = true })";
             "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
             "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-	    "<C-d>" = "cmp.mapping.scroll_docs(4)";
-	    "<C-f>" = "cmp.mapping.scroll_docs(-4)";
-	  };
-	  sources = [
-            { name = "nvim_lsp"; }
-            { name = "path"; }
-            { name = "buffer"; }
-	  ];
-	};
+            "<C-d>" = "cmp.mapping.scroll_docs(4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(-4)";
+          };
+          sources = [
+            {name = "nvim_lsp";}
+            {name = "path";}
+            {name = "buffer";}
+            {name = "treesitter";}
+          ];
+        };
       };
 
       lsp-format.enable = true;
+      none-ls = {
+        enable = true;
+        enableLspFormat = true;
+        sources = {
+          diagnostics = {
+            pylint.enable = true;
+          };
+          formatting = {
+            alejandra.enable = true;
+            black.enable = true;
+          };
+        };
+      };
 
       telescope.enable = true;
-
-      lualine.enable = true;
     };
   };
 }
