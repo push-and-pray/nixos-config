888627ddf1f3646fba0bf8d46b4f192cd7eb81ba
ketamin00 <62392537+ketamin00@users.noreply.github.com>
2024-05-30 20:41:01 +0200

    removed old hyprland cache

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -24,8 +24,8 @@
       experimental-features = "nix-command flakes";
       flake-registry = "";
       nix-path = config.nix.nixPath;
-      substituters = ["https://hyprland.cachix.org" "https://devenv.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="];
+      substituters = ["https://devenv.cachix.org"];
+      trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="];
     };
     channel.enable = false;
     registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
