d24662881ab4a97baa2d96df81735fb592969df0
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-01-16 17:26:06 +0100

    move nixvim config to one place

--- home/julius/shell/nixvim/lsp.nix
+++ home/julius/shell/nixvim/lsp.nix
@@ -1,28 +1,47 @@
-{...}: {
+{pkgs, ...}: {
   programs.nixvim = {
     plugins = {
       otter.enable = true;
       lsp = {
         enable = true;
+        inlayHints = true;
         servers = {
           pyright.enable = true;
           nil_ls.enable = true;
           gopls.enable = true;
+          zls.enable = true;
         };
         keymaps = {
-          silent = true;
+          diagnostic = {
+            "<leader>E" = {
+              action = "open_float";
+              desc = "Show diagnostics";
+            };
+            "<A-k>" = {
+              action = "goto_prev";
+              desc = "Goto Previous Error";
+            };
+            "<A-j>" = {
+              action = "goto_next";
+              desc = "Goto Next Error";
+            };
+          };
           lspBuf = {
+            K = {
+              action = "hover";
+              desc = "Hover";
+            };
             gd = {
               action = "definition";
               desc = "Goto Definition";
             };
-            gr = {
-              action = "references";
-              desc = "Goto References";
-            };
             gD = {
               action = "declaration";
               desc = "Goto Declaration";
+            };
+            gr = {
+              action = "references";
+              desc = "Goto References";
             };
             gI = {
               action = "implementation";
@@ -32,21 +51,18 @@
               action = "type_definition";
               desc = "Type Definition";
             };
-            K = {
-              action = "hover";
-              desc = "Hover";
-            };
-            "<leader>cw" = {
-              action = "workspace_symbol";
-              desc = "Workspace Symbol";
-            };
             "<leader>cr" = {
               action = "rename";
-              desc = "Rename";
+              desc = "Rename Symbol";
+            };
+            "<leader>ca" = {
+              action = "code_action";
+              desc = "Code Action";
             };
           };
         };
       };
+      nvim-lightbulb.enable = true;
 
       none-ls = {
         enable = true;
@@ -55,10 +71,13 @@
         sources = {
           code_actions = {
             statix.enable = true;
+            gitrebase.enable = true;
+            gitsigns.enable = true;
           };
           diagnostics = {
             statix.enable = true;
             yamllint.enable = true;
+            trivy.enable = true;
           };
           formatting = {
             alejandra.enable = true;
@@ -74,6 +93,33 @@
       fidget.enable = true;
 
       lsp-format.enable = true;
+
+      conform-nvim = {
+        enable = true;
+        settings = {
+          formatters_by_ft.rust = ["rustfmt"];
+          format_on_save = {
+            lsp_fallback = "fallback";
+            timeout_ms = 500;
+          };
+          notify_on_error = true;
+        };
+      };
+      rustaceanvim = {
+        enable = true;
+        settings = {
+          tools.enable_clippy = true;
+          server = {
+            default_settings = {
+              inlayHints = {lifetimeElisionHints = {enable = "always";};};
+              rust-analyzer = {
+                cargo = {allFeatures = true;};
+                check = {command = "clippy";};
+              };
+            };
+          };
+        };
+      };
     };
   };
 }
--- hosts/hermes/home.nix
+++ hosts/hermes/home.nix
@@ -7,6 +7,7 @@
 
   imports = [
     ./nixvim
+    ../../home/julius/shell/nixvim
   ];
 
   home = {
