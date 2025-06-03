981e77723e98fddbcb70bb50177a37f323809c84
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-06-03 22:13:21 +0200

    declarative vscode extensions

--- home/julius/desktop/default.nix
+++ home/julius/desktop/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     ./hyprland.nix
     ./waybar.nix
@@ -16,5 +20,23 @@
   ];
 
   programs.firefox.enable = true;
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+    mutableExtensionsDir = false;
+    profiles = {
+      default = {
+        extensions = with pkgs.vscode-extensions; [
+          ms-python.python
+          ms-python.vscode-pylance
+          ms-python.debugpy
+          ms-toolsai.jupyter
+          ms-toolsai.jupyter-keymap
+          ms-toolsai.jupyter-renderers
+          ms-toolsai.vscode-jupyter-cell-tags
+          ms-toolsai.vscode-jupyter-slideshow
+          tomoki1207.pdf
+        ];
+      };
+    };
+  };
 }
