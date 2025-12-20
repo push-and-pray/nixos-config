904c98ca6a6b770c70a461fc12fee6d29aa5a06a
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-12-20 19:33:41 +0100

    fix deprecated attribute

--- system/greetd.nix
+++ system/greetd.nix
@@ -8,7 +8,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time";
         user = "greeter";
       };
     };
